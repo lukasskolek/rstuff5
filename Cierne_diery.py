8993a87db1f5332c27aabc97cdd257e7e122303a
lukasskolek <32292886+lukasskolek@users.noreply.github.com>
2020-10-21 17:20:42 +0200

    Add files via upload

---
+++ Cierne_diery.py
@@ -0,0 +1,53 @@
+import requests
+import re
+import time
+from random import randint
+import smtplib
+from email.message import EmailMessage
+from urllib.request import urlopen
+import json
+import os
+from bs4 import BeautifulSoup as bs
+#print ("done importing")
+cache = []
+
+if os.path.isfile('./cachefile.json'):
+    with open('./cachefile.json') as fh:
+        try:
+            cache = json.load(fh)
+        except json.decoder.JSONDecodeError:
+            os.remove('./cachefile.json')
+url = "https://eshop.ciernediery.sk/"
+page = urlopen(url)
+soup = bs((page.read()),features="lxml")
+
+
+all = [x for x in set(filter(lambda x: 'product' in x,[x.get('href') for x in soup.find_all('a')]))]
+new = [x for x in filter(lambda x: x not in cache, all)]
+cache += new
+grafiky = 'Tieto su nove: \n'
+for x in new-all:
+    grafiky += x + '\n'
+
+print(grafiky)
+
+#with open('./cachefile.json','w') as fh:
+#    json.dump(cache,fh)
+
+#if new:
+#    msg = EmailMessage()
+#    msg.set_content(grafiky)
+#    msg['From'] = 'diery2020cierne@gmail.com'
+#    msg['To'] = 'luky.skolek@gmail.com'
+#    msg['Subject'] = 'Cierne diery dali nove grafiku ty jebko!'
+#    fromaddr = 'diery2020cierne@gmail.com'
+#    toaddrs = ['luky.skolek@gmail.com','filip.alexander.uhlarik@gmail.com']
+#    server = smtplib.SMTP('smtp.gmail.com', 587)
+#    server.starttls()
+#    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+#    server.login('diery2020cierne@gmail.com', '3P6ciaUaLJiraKk')
+#    server.send_message(msg)
+#    server.quit()
+#    response = urlopen(url).read()
+#else:
+#    print('All has been seen')
