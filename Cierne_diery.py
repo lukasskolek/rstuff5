f1defc29b98eff34d93d24b21fe60805621ad933
lukasskolek <32292886+lukasskolek@users.noreply.github.com>
2020-11-03 17:26:27 +0100

    Update Cierne_diery.py

--- Cierne_diery.py
+++ Cierne_diery.py
@@ -36,10 +36,10 @@ if new:
     msg = EmailMessage()
     msg.set_content(grafiky)
     msg['From'] = 'diery2020cierne@gmail.com'
-    msg['To'] = 'luky.skolek@gmail.com'
     msg['Subject'] = 'Cierne diery dali nove grafiku ty jebko!'
     fromaddr = 'diery2020cierne@gmail.com'
-    toaddrs = ['luky.skolek@gmail.com']
+    toaddrs = 'luky.skolek@gmail.com', 'filip.alexander.uhlarik@gmail.com'
+    msg['To'] = ", ".join(toaddrs)
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.starttls()
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
